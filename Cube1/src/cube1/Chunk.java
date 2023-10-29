d66e9f43995f7627013856f731e8107416ac1de9
kendrickofmars <aamukhtar@cpp.edu>
2023-10-28 18:23:01 -0700

    Work-in-progress
    Changes:
    - added slick-util library to project
    -added texture references to chunk class
    TODO:
    -Fix method to load different textures based on blockID
    -add simplex noise references to randomly generate terrain

--- Cube1/src/cube1/Chunk.java
+++ Cube1/src/cube1/Chunk.java
@@ -3,6 +3,10 @@ import java.util.Random;
 import org.lwjgl.BufferUtils;
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL15.*;
+import org.newdawn.slick.opengl.Texture;
+import org.newdawn.slick.opengl.TextureLoader;
+import org.newdawn.slick.util.ResourceLoader;
+
 /**
  *
  * @author fourf
@@ -17,6 +21,8 @@ public class Chunk {
     private int VBOVertexHandle;
     private int VBOColorHandle;
     private int StartX, StartY, StartZ;
+    private int VBOTextureHandle;
+    private Texture texture;
     private Random r;
     
     
@@ -27,36 +33,48 @@ public class Chunk {
         glVertexPointer(3, GL_FLOAT, 0, 0L);
         glBindBuffer(GL_ARRAY_BUFFER,VBOColorHandle);
         glColorPointer(3,GL_FLOAT, 0, 0L);
+        glBindBuffer(GL_ARRAY_BUFFER, VBOTextureHandle);
+        glBindTexture(GL_TEXTURE_2D, 1);
+        glTexCoordPointer(2,GL_FLOAT,0,0L);
         glDrawArrays(GL_QUADS, 0,CHUNK_SIZE *CHUNK_SIZE*CHUNK_SIZE * 24);
     glPopMatrix();
     }
     
-    public void rebuildMesh(
-        float startX, float startY, float startZ) {
+    public void rebuildMesh(float startX, float startY, float startZ) {
         VBOColorHandle = glGenBuffers();
         VBOVertexHandle = glGenBuffers();
+        VBOTextureHandle = glGenBuffers(); 
         FloatBuffer VertexPositionData = BufferUtils.createFloatBuffer((CHUNK_SIZE * CHUNK_SIZE * CHUNK_SIZE) * 6 * 12);
-        FloatBuffer VertexColorData =
-        BufferUtils.createFloatBuffer((CHUNK_SIZE* CHUNK_SIZE * CHUNK_SIZE) * 6 * 12);
+        FloatBuffer VertexColorData = BufferUtils.createFloatBuffer((CHUNK_SIZE* CHUNK_SIZE * CHUNK_SIZE) * 6 * 12);
+        FloatBuffer VertexTextureData = BufferUtils.createFloatBuffer((CHUNK_SIZE* CHUNK_SIZE *CHUNK_SIZE)* 6 * 12);
+
         for (float x = 0; x < CHUNK_SIZE; x += 1) {
             for (float z = 0; z < CHUNK_SIZE; z += 1) {
                 for(float y = 0; y < CHUNK_SIZE; y++){
                     VertexPositionData.put(createCube((float) (startX + x * CUBE_LENGTH), (float)(y*CUBE_LENGTH + (int)(CHUNK_SIZE*.8)),
                                                         (float) (startZ + z *CUBE_LENGTH)));
                     VertexColorData.put(createCubeVertexCol(getCubeColor(Blocks[(int) x][(int) y][(int) z])));
+                    VertexTextureData.put(createTexCube((float) 0, (float) 0, Blocks[(int)(x)][(int) (y)][(int) (z)]));
 
 
                 }
             }
         }
+        
+        VertexTextureData.flip();
         VertexColorData.flip();
         VertexPositionData.flip();
+        
         glBindBuffer(GL_ARRAY_BUFFER,VBOVertexHandle);
         glBufferData(GL_ARRAY_BUFFER,VertexPositionData,GL_STATIC_DRAW);
         glBindBuffer(GL_ARRAY_BUFFER, 0);
         glBindBuffer(GL_ARRAY_BUFFER,VBOColorHandle);
         glBufferData(GL_ARRAY_BUFFER,VertexColorData,GL_STATIC_DRAW);
         glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindBuffer(GL_ARRAY_BUFFER, VBOTextureHandle);
+        glBufferData(GL_ARRAY_BUFFER, VertexTextureData,GL_STATIC_DRAW);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+
         }
     private float[] createCubeVertexCol(float[] CubeColorArray) {
         float[] cubeColors = new float[CubeColorArray.length * 4 * 6];
@@ -69,49 +87,97 @@ public class Chunk {
     public static float[] createCube(float x, float y,float z) {
         int offset = CUBE_LENGTH / 2;
         return new float[] {
-        // TOP QUAD
-        x + offset, y + offset, z,
-        x - offset, y + offset, z,
-        x - offset, y + offset, z - CUBE_LENGTH,
-        x + offset, y + offset, z - CUBE_LENGTH,
-        // BOTTOM QUAD
-        x + offset, y - offset, z - CUBE_LENGTH,
-        x - offset, y - offset, z - CUBE_LENGTH,
-        x - offset, y - offset, z,
-        x + offset, y - offset, z,
-        // FRONT QUAD
-        x + offset, y + offset, z - CUBE_LENGTH,
-        x - offset, y + offset, z - CUBE_LENGTH,
-        x - offset, y - offset, z - CUBE_LENGTH,    
-        x + offset, y - offset, z - CUBE_LENGTH,
-        // BACK QUAD
-        x + offset, y - offset, z,
-        x - offset, y - offset, z,
-        x - offset, y + offset, z,
-        x + offset, y + offset, z,
-        // LEFT QUAD
-        x - offset, y + offset, z - CUBE_LENGTH,
-        x - offset, y + offset, z,
-        x - offset, y - offset, z,
-        x - offset, y - offset, z - CUBE_LENGTH,
-        // RIGHT QUAD
-        x + offset, y + offset, z,
-        x + offset, y + offset, z - CUBE_LENGTH,
-        x + offset, y - offset, z - CUBE_LENGTH,
-        x + offset, y - offset, z };
+            // TOP QUAD
+            x + offset, y + offset, z,
+            x - offset, y + offset, z,
+            x - offset, y + offset, z - CUBE_LENGTH,
+            x + offset, y + offset, z - CUBE_LENGTH,
+            // BOTTOM QUAD
+            x + offset, y - offset, z - CUBE_LENGTH,
+            x - offset, y - offset, z - CUBE_LENGTH,
+            x - offset, y - offset, z,
+            x + offset, y - offset, z,
+            // FRONT QUAD
+            x + offset, y + offset, z - CUBE_LENGTH,
+            x - offset, y + offset, z - CUBE_LENGTH,
+            x - offset, y - offset, z - CUBE_LENGTH,    
+            x + offset, y - offset, z - CUBE_LENGTH,
+            // BACK QUAD
+            x + offset, y - offset, z,
+            x - offset, y - offset, z,
+            x - offset, y + offset, z,
+            x + offset, y + offset, z,
+            // LEFT QUAD
+            x - offset, y + offset, z - CUBE_LENGTH,
+            x - offset, y + offset, z,
+            x - offset, y - offset, z,
+            x - offset, y - offset, z - CUBE_LENGTH,
+            // RIGHT QUAD
+            x + offset, y + offset, z,
+            x + offset, y + offset, z - CUBE_LENGTH,
+            x + offset, y - offset, z - CUBE_LENGTH,
+            x + offset, y - offset, z };
         }
     private float[] getCubeColor(Block block) {
-    switch (block.GetID()) {
-        case 1:
-        return new float[] { 0, 1, 0 };
-        case 2:
-        return new float[] { 1, 0.5f, 0 };
-        case 3:
-        return new float[] { 0, 0f, 1f };
-        }
+//    switch (block.GetID()) {
+//        case 1:
+//        return new float[] { 0, 1, 0 };
+//        case 2:
+//        return new float[] { 1, 0.5f, 0 };
+//        case 3:
+//        return new float[] { 0, 0f, 1f };
+//        }
         return new float[] { 1, 1, 1 };
     }
+    
+    public static float[] createTexCube(float x, float y, Block block) {
+        float offset = (1024f/16)/1024f;
+        switch (block.GetID()) { //producing different textures based on what ID is passed 
+            case 1:
+                return new float[] {
+                    // BOTTOM QUAD(DOWN=+Y)
+                    x + offset*3, y + offset*10,
+                    x + offset*2, y + offset*10,
+                    x + offset*2, y + offset*9,
+                    x + offset*3, y + offset*9,
+                    // TOP!
+                    x + offset*3, y + offset*1,
+                    x + offset*2, y + offset*1,
+                    x + offset*2, y + offset*0,
+                    x + offset*3, y + offset*0,
+                    // FRONT QUAD
+                    x + offset*3, y + offset*0,
+                    x + offset*4, y + offset*0,
+                    x + offset*4, y + offset*1,
+                    x + offset*3, y + offset*1,
+                    // BACK QUAD
+                    x + offset*4, y + offset*1,
+                    x + offset*3, y + offset*1,
+                    x + offset*3, y + offset*0,
+                    x + offset*4, y + offset*0,
+                    // LEFT QUAD
+                    x + offset*3, y + offset*0,
+                    x + offset*4, y + offset*0,
+                    x + offset*4, y + offset*1,
+                    x + offset*3, y + offset*1,
+                    // RIGHT QUAD
+                    x + offset*3, y + offset*0,
+                    x + offset*4, y + offset*0,
+                    x + offset*4, y + offset*1,
+                    x + offset*3, y + offset*1};
+        }
+        
+        
+    }      
+
     public Chunk(int startX, int startY, int startZ) {
+        try{
+            texture = TextureLoader.getTexture("PNG",ResourceLoader.getResourceAsStream("terrain.png"));
+        }
+        catch(Exception e){
+            System.out.print("ER-ROAR!");
+        }
+
         r= new Random();
         Blocks = new Block[CHUNK_SIZE][CHUNK_SIZE][CHUNK_SIZE];
         for (int x = 0; x < CHUNK_SIZE; x++) {
@@ -133,6 +199,7 @@ public class Chunk {
         }
         VBOColorHandle = glGenBuffers();
         VBOVertexHandle = glGenBuffers();
+        VBOTextureHandle = glGenBuffers();
         StartX = startX;
         StartY = startY;
         StartZ = startZ;
